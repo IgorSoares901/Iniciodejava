79ecf7c934297bebea715a9e43702f3b1d47603b
IgorSoares901 <thelasthunter782@gmail.com>
2025-03-12 20:58:33 -0300

    Add files via upload

---
+++ TreinandoAula1/src/treinandoaula1/TreinandoAula1.java
@@ -0,0 +1,27 @@
+
+package treinandoaula1;
+
+
+public class TreinandoAula1 {
+
+   
+    public static void main(String[] args) {
+        int A,B,C,D,E,F,G;
+        A = 6;
+        B = 5 + 5;
+        C = A + B;
+        D = A + B + 20;
+        E = A * B;
+        F = C / A;
+        G = A / 2;
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+        System.out.println("C = " + C);
+        System.out.println("D = " + D);
+        System.out.println("E = " + E);
+        System.out.println("F = " + F);
+        System.out.println("G = " + G);
+              
+    }
+    
+}
